72c26f5fe52989e237f5fbcde5cf678a699d4ff6
Alfred Tenda Lumataw <alfrdt87@gmail.com>
2022-12-15 01:11:03 +0800

    tambah link di footer

--- darmisa.php
+++ darmisa.php
@@ -571,21 +571,21 @@
 
 
 
-    <!-- ========== footer ========== -->
-        <div class="container-fluid bg-light">
+     <!-- ========== footer ========== -->
+     <div class="container-fluid bg-light">
             <footer class="footer py-5 " style="font-family: sans-serif;">
                 <div class="row">
                     <div class="justify-content-between col-12 col-md-4 mb-3">
                         <div class="row">
-                            <a class="navbar-brand navbar-dark d-flex p-0 align-items-center text-dark w-75 fs-5 ms-sm-3 col-6 col-md-4 me-6 mb-4" href="./darmisa.php"></svg><img class="logo-darmisa text-dark" src="./image/logo-seal.jpeg" alt="" title="Social Economic Accelerator Lab"><span class="fw-bold mx-4" title="DarMisa">DarMisa</span></a>
+                            <a class="navbar-brand navbar-dark d-flex p-0 align-items-center text-dark w-75 fs-5 ms-sm-3 col-6 col-md-4 me-6 mb-4" href="darmisa.php"></svg><img class="logo-darmisa text-dark" src="./image/logo-seal.jpeg" alt="" title="Social Economic Accelerator Lab"><span a class="fw-bold mx-4" title="DarMisa">DarMisa</span></a>
                         </div>
                         
                         <p class="mb-4">Damkar Information And Reporting Website</p>
                         <p class="fw-bold mb-1">Follow Us</p>
                         <ul class="nav list-unstyled">
                             <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"></a></li>
-                            <li class=""><a class="text-muted mb-2" href="#"><i class="bi bi-facebook" title="facebook"></i></a></li>
-                            <li class="ms-2"><a class="text-muted mb-1" href="#"><i class="bi bi-whatsapp" title="Whatsapp"></i></a></li>
+                            <li class=""><a class="text-muted mb-2" href="https://web.facebook.com/dinasdamkar.kabupatenminahasa"><i class="bi bi-facebook" title="facebook"></i></a></li>
+                            <!-- <li class="ms-2"><a class="text-muted mb-1" href="https://wa.me/message/XUYOEQ6PQY7SF1"><i class="bi bi-whatsapp" title="Whatsapp"></i></a></li> -->
                             <li class="ms-2"><a class="text-muted mb-1" href="#"><i class="bi bi-instagram" title="instagram"></i></a></li>
                             <li class="ms-2"><a class="text-muted mb-1" href="#"><i class="bi bi-youtube" title="Youtube"></i></a></li>
                         </ul>
@@ -596,7 +596,7 @@
                         <ul class="nav flex-column">
                             <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted lh-lg mb-3 me-4"><i class="bi bi-geo-alt"></i> 8W77+JJ, Kembuan, Kec. Tondano Utara, Kabupaten Minahasa, Sulawesi utara</a></li>
                             <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted mb-3"><i class="bi bi-telephone"></i> (0431) 321001</a></li>
-                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><i class="bi bi-envelope"></i> darmisa@gmail.com</a></li>
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><i class="bi bi-envelope"></i> darmisa.minahasa@gmail.com</a></li>
                         </ul>
                     </div>
 
@@ -611,12 +611,12 @@
                     </div> -->
           
                     <div class="justify-content-end col-12 col-md-4 mb-3">
-                        <form>
-                        <h5 class="mb-4">Critism And Suggestion</h5>
-                            <div class="d-flex flex-column flex-sm-row list-unstyled w-100 gap-2">
+                        <form action="mailto:darmisa.minahasa@gmail.com" enctype="text/plain" method="post">                        
+                            <h5 class="mb-4">Critism And Suggestion</h5>
+                            <div class="d-flex flex-column flex-sm-row list-unstyled w-100 gap-2 w-100">
                                 <label for="newsletter1" class="visually-hidden">comment</label>
-                                <input type="text" class="footer-form text-center" style="border-radius: 25px 0 0 25px;" placeholder="Text Here">
-                                <button class="btn btn-white bg-dark text-white" style="border-radius: 0 25px 25px 0; margin-left: -10px;" type="button"><a class="text-muted mb-2" href="#"></a><i class="bi bi-send"></i></button>
+                                <!-- <input type="text" class="footer-form text-center" style="border-radius: 25px 0 0 25px;" placeholder="Text Here"> -->
+                                <button type="submit" class="btn btn-white bg-dark text-white w-50" style="border-radius: 25px 25px 25px 25px; margin-left: -10px;"><a class="text-muted mb-2" href="alfrdt87@gmail.com"></a>Send <i class="bi bi-send"></i></button>
                             </div>
                         </form>
                     </div>
